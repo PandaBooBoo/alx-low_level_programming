a2b03e5ea6e40f68c875cb46502247424296ee23
PandaBooBoo <aaronchinzila@gmail.com>
2023-06-29 11:19:40 -0700

    q0 fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,18 +8,22 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int i;
-    int n = 0;
+	int i;
+	int j;
 
-    while (dest[n] != '\0')
-    {
-        n++;
-    }
-    for (i = 0 ; i < n && src[i] != '\0' ; i++)
-    {
-        dest[n + i] = src[i];
-    }
-    dest[n + i] = '\0';
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-    return (dest);
+	dest[i] = '\0';
+	return (dest);
 }
