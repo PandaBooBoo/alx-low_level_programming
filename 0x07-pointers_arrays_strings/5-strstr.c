6a4a9f024c5a0eec69690dc8284a0b174dd3d60a
PandaBooBoo <aaronchinzila@gmail.com>
2023-07-03 11:52:39 -0700

    q5 fix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,30 +9,20 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j, start;
-	int nlen = 0;
-
-	i = 0;
-	j = 0;
-	start = 0;
-
-	while (needle[nlen] != '\0')
+	for (; *haystack != '\0'; haystack++)
 	{
-		nlen++;
-	}
-	for (i = 0; haystack[i] != '\0'; i++)
-	{
-		for (j = 0; j < nlen && haystack[i] == needle[j]; j++, i++)
+		char *l = haystack;
+		char *p = needle;
+
+		while (*l == *p && *p != '\0')
 		{
-			if (j == 0)
-			{
-				start = i;
-			}
-			if (j == nlen - 1)
-			{
-				return (haystack + start);
-			}
+			l++;
+			p++;
 		}
+
+		if (*p == '\0')
+			return (haystack);
 	}
+
 	return (0);
 }
