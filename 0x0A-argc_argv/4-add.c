150aeecf888d23e454753458703b3832ce9b8524
PandaBooBoo <aaronchinzila@gmail.com>
2023-07-07 12:24:35 -0700

    q4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,66 +1,71 @@
-#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <ctype.h>
+#include <string.h>
 /**
- * is_number - checks to see if input is a number
- * @s: input to check for numberhood
- * Return: 1 if it is a number, 0 if not
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return: Always 0 (Success)
  */
-
-int is_number(char *s)
+int check_num(char *str)
 {
-	int i;
+	/*Declaring variables*/
+	unsigned int count;
 
-	i = 0;
-	while (*(s + i) != '\0')
+	count = 0;
+	while (count < strlen(str)) /*count string*/
+
 	{
-		if (*(s + i) >= '0' && *(s + i) <= '9')
-		{
-			i++;
-		}
-		else
+		if (!isdigit(str[count])) /*check if str there are digit*/
 		{
 			return (0);
 		}
+
+		count++;
 	}
 	return (1);
 }
 
 /**
- * main - prints the sum of positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: (0)
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
-{
-	int i, sum, is_num;
 
-	sum = 0;
-	if (argc == 1)
+{
+
+	/*Declaring variables*/
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+	while (count < argc) /*Goes through the whole array*/
 	{
-		printf("0\n");
-	}
-	else if (argc > 1)
-	{
-		i = 1;
-		while (i < argc)
+		if (check_num(argv[count]))
+
 		{
-			is_num = is_number(argv[i]);
-			if (is_num == 1)
-			{
-				sum += atoi(argv[i]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-			i++;
+			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+			sum += str_to_int;
 		}
-		printf("%d\n", sum);
+
+		/*Condition if one of the number contains symbols that are not digits*/
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		count++;
 	}
+
+	printf("%d\n", sum); /*print sum*/
+
 	return (0);
 }
+
